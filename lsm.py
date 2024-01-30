2c1e7b39b3be7de64725a7f59e2e1ecae511d814
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2024-01-29 18:58:35 -0700

    initial inline documentation

--- lsm.py
+++ lsm.py
@@ -1,4 +1,4 @@
-import discord  # added this
+import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
@@ -9,8 +9,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 WHITELIST = os.getenv('WHITELIST').split(",")
 
 prefix = "/"
-intents = discord.Intents.all()  # added this
-bot = commands.Bot(command_prefix=prefix, intents=intents)  # modified this
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 
 @bot.event
@@ -18,29 +18,26 @@ async def on_ready():
     print("LSM is online and ready to recieve commands")
 
 
+#the bot waits for messages and parses them through this function
 @bot.event
 async def on_message(message):
-    print("The message's author was", message.author)
-    print(str(message.author) in WHITELIST)
-    await bot.process_commands(message)
+    #only whitelisted users are allowed to access the bot commands
+    if (str(message.author) in WHITELIST):
+        await bot.process_commands(message)
 
 
+#command to get the ping for the bot to the discord server
 @bot.command()
 async def ping(ctx):
-    '''
-    This text will be shown in the help command
-    '''
-
-    print("hello")
-
-    # Get the latency of the bot
-    latency = bot.latency  # Included in the Discord.py library
-    # Send it to the user
+    print("ping info requested")
+    latency = bot.latency
     await ctx.send(latency)
 
 
+#basic echo command for the bot
 @bot.command()
 async def echo(ctx, *, content:str):
     await ctx.send(content)
 
-bot.run(TOKEN)  # Where 'TOKEN' is your bot token
+#run the bot
+bot.run(TOKEN)
