0e42550ac9abde04a39696827c2295e619efb1cd
antoniolanza <antoniolanza@shaw.ca>
2024-01-30 11:02:37 -0700

    Dockerized version
    [ADD] DockerFile for building container and running
    [ADD] reqirements.txt for pip dependecies
    [UPDATE] lsm.py load env variables without dotenv, not working docker commands to be replaced with request apis

--- lsm.py
+++ lsm.py
@@ -1,9 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
-
-load_dotenv('./.env')
+import subprocess
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 WHITELIST = os.getenv('WHITELIST').split(",")
@@ -11,7 +9,7 @@ WHITELIST = os.getenv('WHITELIST').split(",")
 prefix = "/"
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=prefix, intents=intents)
-
+pal_dir = "/docker-compose-palworld"
 
 @bot.event
 async def on_ready():
@@ -38,6 +36,32 @@ async def ping(ctx):
 @bot.command()
 async def echo(ctx, *, content:str):
     await ctx.send(content)
+
+@bot.command()
+async def palworld_shutdown(ctx):
+    try:
+        result = subprocess.run(
+            ["docker", "compose", "down"], 
+            cwd=pal_dir, 
+            check=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print("debug")
+        print("Output:", result.stdout)
+        print("Error:", result.stderr)
+    except subprocess.CalledProcessError as e:
+        print("Failed to run docker-compose down:", e)
+
+@bot.command()
+async def palworld_startup(ctx):
+    try:
+        pal_dir = '/docker-compose-palworld'
+        subprocess.run(["docker","compose","up", "-d"], cwd=pal_dir,check=True)
+        await ctx.send("Starting up The Palworld Server")
+    except subprocess.CalledProcessError as e:
+        await ctx.send(f"Failed to Startup Palworld Server: {e}")
 
 #run the bot
 bot.run(TOKEN)
